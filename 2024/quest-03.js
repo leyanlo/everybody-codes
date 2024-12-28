c4e3798ca1aaca51c550e6a4f887be3de191ae1b
Leyan Lo <leyanlo@gmail.com>
2024-12-28 00:22:58 -0800

    chore(template): update template

--- 2024/quest-03.js
+++ 2024/quest-03.js
@@ -1,13 +1,11 @@
+import { readFileSync } from 'node:fs';
+
 var input = ``;
-var part = 1;
+// var input = readFileSync('./quest-03-input1.txt', 'utf8').trimEnd();
+// var input = readFileSync('./quest-03-input2.txt', 'utf8').trimEnd();
+// var input = readFileSync('./quest-03-input3.txt', 'utf8').trimEnd();
 
-// var input = ``;
-// var part = 2;
-
-// var input = ``;
-// var part = 3;
-
-function solve(input, part) {
+function solve(input) {
   console.log(input);
 }
-solve(input, part);
+solve(input);
--- quest-template.js
+++ quest-template.js
@@ -1,13 +1,11 @@
+import { readFileSync } from 'node:fs';
+
 var input = ``;
-var part = 1;
+// var input = readFileSync('./quest-xx-input1.txt', 'utf8').trimEnd();
+// var input = readFileSync('./quest-xx-input2.txt', 'utf8').trimEnd();
+// var input = readFileSync('./quest-xx-input3.txt', 'utf8').trimEnd();
 
-// var input = ``;
-// var part = 2;
-
-// var input = ``;
-// var part = 3;
-
-function solve(input, part) {
+function solve(input) {
   console.log(input);
 }
-solve(input, part);
+solve(input);
